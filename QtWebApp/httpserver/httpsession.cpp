7b1ec1f9c902441d56c02c75993157f6fd63d92e
Carlos Alves <carlosb.alves@pm.me>
2022-01-30 15:12:11 -0300

    Changes in HttpSession
    Class members names
    Class initialize members

--- QtWebApp/httpserver/httpsession.cpp
+++ QtWebApp/httpserver/httpsession.cpp
@@ -11,51 +11,51 @@ using namespace stefanfrings;
 
 HttpSession::HttpSession( bool canStore ) {
     if ( canStore ) {
-        dataPtr=new HttpSessionData();
-        dataPtr->refCount=1;
-        dataPtr->lastAccess=QDateTime::currentMSecsSinceEpoch();
-        dataPtr->id=QUuid::createUuid().toString().toLocal8Bit();
+        m_dataPtr = new HttpSessionData();
+        m_dataPtr->refCount=1;
+        m_dataPtr->lastAccess=QDateTime::currentMSecsSinceEpoch();
+        m_dataPtr->id=QUuid::createUuid().toString().toLocal8Bit();
 #ifdef SUPERVERBOSE
-        qDebug( "HttpSession: (constructor) new session %s with refCount=1", dataPtr->id.constData() );
+        qDebug( "HttpSession: (constructor) new session %s with refCount=1", m_dataPtr->id.constData() );
 #endif
-    }else {
-        dataPtr=nullptr;
+    } else {
+        m_dataPtr = nullptr;
     }
 }
 
 HttpSession::HttpSession( const HttpSession& other ) {
-    dataPtr=other.dataPtr;
-    if ( dataPtr ) {
-        dataPtr->lock.lockForWrite();
-        dataPtr->refCount++;
+    m_dataPtr = other.m_dataPtr;
+    if ( m_dataPtr ) {
+        m_dataPtr->lock.lockForWrite();
+        m_dataPtr->refCount++;
 #ifdef SUPERVERBOSE
-        qDebug( "HttpSession: (constructor) copy session %s refCount=%i", dataPtr->id.constData(), dataPtr->refCount );
+        qDebug( "HttpSession: (constructor) copy session %s refCount=%i", m_dataPtr->id.constData(), m_dataPtr->refCount );
 #endif
-        dataPtr->lock.unlock();
+        m_dataPtr->lock.unlock();
     }
 }
 
 HttpSession& HttpSession::operator= ( const HttpSession& other ) {
-    HttpSessionData* oldPtr=dataPtr;
-    dataPtr=other.dataPtr;
-    if ( dataPtr ) {
-        dataPtr->lock.lockForWrite();
-        dataPtr->refCount++;
+    HttpSessionData* oldPtr = m_dataPtr;
+    m_dataPtr = other.m_dataPtr;
+    if ( m_dataPtr ) {
+        m_dataPtr->lock.lockForWrite();
+        m_dataPtr->refCount++;
 #ifdef SUPERVERBOSE
-        qDebug( "HttpSession: (operator=) session %s refCount=%i", dataPtr->id.constData(), dataPtr->refCount );
+        qDebug( "HttpSession: (operator=) session %s refCount=%i", m_dataPtr->id.constData(), m_dataPtr->refCount );
 #endif
-        dataPtr->lastAccess=QDateTime::currentMSecsSinceEpoch();
-        dataPtr->lock.unlock();
+        m_dataPtr->lastAccess=QDateTime::currentMSecsSinceEpoch();
+        m_dataPtr->lock.unlock();
     }
     if ( oldPtr ) {
         int refCount;
         oldPtr->lock.lockForWrite();
-        refCount=--oldPtr->refCount;
+        refCount = --oldPtr->refCount;
 #ifdef SUPERVERBOSE
         qDebug( "HttpSession: (operator=) session %s refCount=%i", oldPtr->id.constData(), oldPtr->refCount );
 #endif
         oldPtr->lock.unlock();
-        if ( refCount==0 ) {
+        if ( refCount == 0 ) {
             qDebug( "HttpSession: deleting old data" );
             delete oldPtr;
         }
@@ -64,93 +64,99 @@ HttpSession& HttpSession::operator= ( const HttpSession& other ) {
 }
 
 HttpSession::~HttpSession() {
-    if ( dataPtr ) {
+    if ( m_dataPtr ) {
         int refCount;
-        dataPtr->lock.lockForWrite();
-        refCount=--dataPtr->refCount;
+        m_dataPtr->lock.lockForWrite();
+        refCount = --m_dataPtr->refCount;
 #ifdef SUPERVERBOSE
         qDebug( "HttpSession: (destructor) session %s refCount=%i", dataPtr->id.constData(), dataPtr->refCount );
 #endif
-        dataPtr->lock.unlock();
-        if ( refCount==0 ) {
+        m_dataPtr->lock.unlock();
+        if ( refCount == 0 ) {
+#ifdef SUPERVERBOSE
             qDebug( "HttpSession: deleting data" );
-            delete dataPtr;
+#endif
+            delete m_dataPtr;
         }
     }
 }
 
 QByteArray HttpSession::getId() const {
-    if ( dataPtr ) {
-        return dataPtr->id;
-    }else {
-        return QByteArray();
+    if ( m_dataPtr ) {
+        return m_dataPtr->id;
     }
+
+    return QByteArray();
 }
 
 bool HttpSession::isNull() const {
-    return dataPtr==nullptr;
+    return m_dataPtr == nullptr;
 }
 
 void HttpSession::set( const QByteArray& key, const QVariant& value ) {
-    if ( dataPtr ) {
-        dataPtr->lock.lockForWrite();
-        dataPtr->values.insert( key, value );
-        dataPtr->lock.unlock();
+    if ( m_dataPtr ) {
+        m_dataPtr->lock.lockForWrite();
+        m_dataPtr->values.insert( key, value );
+        m_dataPtr->lock.unlock();
     }
 }
 
 void HttpSession::remove( const QByteArray& key ) {
-    if ( dataPtr ) {
-        dataPtr->lock.lockForWrite();
-        dataPtr->values.remove( key );
-        dataPtr->lock.unlock();
+    if ( m_dataPtr ) {
+        m_dataPtr->lock.lockForWrite();
+        m_dataPtr->values.remove( key );
+        m_dataPtr->lock.unlock();
     }
 }
 
 QVariant HttpSession::get( const QByteArray& key ) const {
     QVariant value;
-    if ( dataPtr ) {
-        dataPtr->lock.lockForRead();
-        value=dataPtr->values.value( key );
-        dataPtr->lock.unlock();
+    if ( m_dataPtr ) {
+        m_dataPtr->lock.lockForRead();
+        value = m_dataPtr->values.value( key );
+        m_dataPtr->lock.unlock();
     }
+
     return value;
 }
 
 bool HttpSession::contains( const QByteArray& key ) const {
     bool found=false;
-    if ( dataPtr ) {
-        dataPtr->lock.lockForRead();
-        found=dataPtr->values.contains( key );
-        dataPtr->lock.unlock();
+    if ( m_dataPtr ) {
+        m_dataPtr->lock.lockForRead();
+        found = m_dataPtr->values.contains( key );
+        m_dataPtr->lock.unlock();
     }
+
     return found;
 }
 
 QMap<QByteArray, QVariant> HttpSession::getAll() const {
     QMap<QByteArray, QVariant> values;
-    if ( dataPtr ) {
-        dataPtr->lock.lockForRead();
-        values=dataPtr->values;
-        dataPtr->lock.unlock();
+    if ( m_dataPtr ) {
+        m_dataPtr->lock.lockForRead();
+        values = m_dataPtr->values;
+        m_dataPtr->lock.unlock();
     }
+
     return values;
 }
 
 qint64 HttpSession::getLastAccess() const {
-    qint64 value=0;
-    if ( dataPtr ) {
-        dataPtr->lock.lockForRead();
-        value=dataPtr->lastAccess;
-        dataPtr->lock.unlock();
+    qint64 value = 0;
+    if ( m_dataPtr ) {
+        m_dataPtr->lock.lockForRead();
+        value = m_dataPtr->lastAccess;
+        m_dataPtr->lock.unlock();
     }
+
     return value;
 }
 
 void HttpSession::setLastAccess() {
-    if ( dataPtr ) {
-        dataPtr->lock.lockForWrite();
-        dataPtr->lastAccess=QDateTime::currentMSecsSinceEpoch();
-        dataPtr->lock.unlock();
+    if ( m_dataPtr ) {
+        m_dataPtr->lock.lockForWrite();
+        m_dataPtr->lastAccess = QDateTime::currentMSecsSinceEpoch();
+        m_dataPtr->lock.unlock();
     }
 }
--- QtWebApp/httpserver/httpsession.h
+++ QtWebApp/httpserver/httpsession.h
@@ -29,7 +29,7 @@ public:
        @param canStore The session can store data, if this parameter is true.
        Otherwise all calls to set() and remove() do not have any effect.
      */
-    HttpSession( const bool canStore=false );
+    HttpSession( const bool canStore = false );
 
     /**
        Copy constructor. Creates another HttpSession object that shares the
@@ -112,7 +112,7 @@ private:
     };
 
     /** Pointer to the shared data. */
-    HttpSessionData* dataPtr;
+    HttpSessionData* m_dataPtr;
 
 };
 
